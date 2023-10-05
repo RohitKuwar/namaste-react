f9c50a21c6ea041b24ee3bd035d7e3e687c7a9f8
RohitKuwar <rohit1kuwar@gmail.com>
2023-10-05 11:13:06 +0530

    Revert "Chapter 8 - Optimising our App"
    This reverts commit 199af22f1fc0dfd1e4cc27349366b66dab44a343.

--- 9-Optimising our App/Theory.md
+++
@@ -1,2 +0,0 @@
-## Namaste React Course by Akshay Saini
-# Chapter 09 - Optimising our App
--- 9-Optimising our App/src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const NAV_LOGO = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Burger.png'
-
-export const CDN_IMG_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
-
-export const MENU_URL = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.0403896&lng=72.5160459&restaurantId='
