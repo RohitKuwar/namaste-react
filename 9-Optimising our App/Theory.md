fc1ba35da391a9e239cbdde48dd79a8891c08ed1
RohitKuwar <rohit1kuwar@gmail.com>
2023-10-05 11:34:00 +0530

    Chapter 9 - Optimising our App

---
+++ 9-Optimising our App/Theory.md
@@ -0,0 +1,2 @@
+## Namaste React Course by Akshay Saini
+# Chapter 09 - Optimising our App
---
+++ 9-Optimising our App/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header'
+import useOnlineStatus from '../utils/useOnlineStatus';
+
+function Contact() {
+  const status = useOnlineStatus()
+
+  console.log('status', status)
+
+  return (
+    <div>
+        <Header />
+        <h1>Contact</h1>
+        <h3>rohit</h3>
+        <h2>{status.toString()}</h2>
+    </div>
+  )
+}
+
+export default Contact
